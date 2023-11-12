30961243717d548ca76e82557e15b458d109f595
b-knd <angjingru@gmail.com>
2023-11-12 21:29:37 +0800

    Status: Functional calculator with basic functions working, including history Changes - Debug x and divide using the symbol instead of * and / - Use math.json library for expression evaluation - Added limit for history, clearing old history that exceed the limit - Edit site.css and calculator.css to enhance view Changes to add - The rest of the functions - If can change the border to black instead of white (still can't find where it is) - Add comment for readability

--- ScientificCalculator/wwwroot/js/calculator.js
+++ ScientificCalculator/wwwroot/js/calculator.js
@@ -1,6 +1,5 @@
-﻿// calculator.js
-
-document.addEventListener('DOMContentLoaded', function () {
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Constant variables to store all the elements
     const buttons = document.querySelectorAll('.buttons button');
     const inputEl = document.getElementById('input');
     const outputEl = document.getElementById('output');
@@ -12,10 +11,14 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem(STORAGE_NAME, JSON.stringify([]));
     }
 
+    // Create a math.js instance
+    const mathPro = math.create();
+
     refreshHistory();
 
     for (let button of buttons) {
-        button.addEventListener('click', function () {
+        // Add listener to function buttons
+        button.addEventListener('pointerdown', function () {
             const symbol = button.innerText;
 
             if (symbol === '=') {
@@ -24,15 +27,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 inputEl.value = inputEl.value.slice(0, -1);
             } else if (symbol === 'CLR') {
                 inputEl.value = '';
+                outputEl.value = '';
             } else {
                 inputEl.value += symbol;
             }
         });
     }
 
+    // Function to be called when '=' is clicked on
+    // Basically, evaluate the input and show the result as output
     function evaluateExpression() {
         try {
-            const result = eval(inputEl.value);
+            // Replace 'x' with '*' before evaluation
+            const expression = preprocessInput(inputEl.value);
+            const result = mathPro.evaluate(expression);
             outputEl.value = result;
             saveToHistory(inputEl.value, result);
             refreshHistory();
@@ -41,18 +49,30 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function preprocessInput(input) {
+        // Replace 'x' with '*' and '÷' with '/'
+        return input.replace(/x/g, '*').replace(/÷/g, '/');
+    }
+
+    // When an output is evaluated, save expression and output to history
     function saveToHistory(expression, result) {
         const historyElements = JSON.parse(localStorage.getItem(STORAGE_NAME)) || [];
         historyElements.push({ expression, result });
         localStorage.setItem(STORAGE_NAME, JSON.stringify(historyElements));
     }
 
+    // Function to refresh history, limiting the number of history displayed as 9
     function refreshHistory() {
+        const MAX_HISTORY_ITEMS = 9; // Set the maximum number of history items to display
+
         historyContainer.innerHTML = '';
 
         const historyElements = JSON.parse(localStorage.getItem(STORAGE_NAME)) || [];
 
-        for (let i = historyElements.length - 1; i >= 0; i--) {
+        // Calculate the starting index to display based on the maximum history items
+        const startIndex = Math.max(0, historyElements.length - MAX_HISTORY_ITEMS);
+
+        for (let i = historyElements.length - 1; i >= startIndex; i--) {
             const div = document.createElement('div');
             div.className = 'historyItem';
 
@@ -63,9 +83,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
             historyContainer.appendChild(div);
 
-            div.addEventListener('click', function () {
+            div.addEventListener('pointerdown', function () {
                 inputEl.value = historyElements[i].expression;
             });
+        }
+
+        // Clear old history items beyond the maximum limit
+        if (historyElements.length > MAX_HISTORY_ITEMS) {
+            const newHistory = historyElements.slice(startIndex);
+            localStorage.setItem(STORAGE_NAME, JSON.stringify(newHistory));
         }
     }
 
